42e454b97140025d791fbc8ad87e40de003dc241
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-23 11:45:46 -0300

    paginas e links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Produtos from './Components/Produtos';
+import Contato from './Components/Contato';
+import './App.css';
 
 function App() {
   return (
-    <div >
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
